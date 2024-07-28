d16c748b57d2492bf6f143d3b5338c1abcaaed78
Vishal Singh <mailtovishalsingh30@gmail.com>
2024-07-28 14:10:51 +0530

    Style Updates

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -4,7 +4,7 @@ import TaskCard from "./TaskCard";
 function TaskColumn({title, icon, tasks, status, handleTaskDelete}){
     return (
         <div className="task_column">
-            <h2 className="task_column_heading"><img className="task_column_icon" src={icon} alt="Check img" />{title}</h2>
+            <h1 className="task_column_heading"><img className="task_column_icon" src={icon} alt="Check img" />{title}</h1>
             {
                 tasks.map((task, index) => {
                     return task.status === status && <TaskCard key={index} title={task.task} tags = {task.tags} handleTaskDelete={handleTaskDelete} index={index}/>
